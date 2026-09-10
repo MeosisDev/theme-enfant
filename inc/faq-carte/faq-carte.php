b235f0777da6f8dcf19ee8774b90bc59183f21d0
Cedric HOTZ <chotz@ads.local>
2026-09-10 13:59:06 +0200

    Ajout de l'onglet dans les réglages FAQ

--- inc/faq-carte/faq-carte.php
+++ inc/faq-carte/faq-carte.php
@@ -21,6 +21,13 @@ function faq_acf_local_fields() {
         'title' => 'FAQ cartes à retourner',
         'fields' => array(
             array(
+                'key' => 'field_faq_tab_carte',
+                'label' => 'FAQ Cartes',
+                'name' => '',
+                'type' => 'tab',
+                'placement' => 'top',
+            ),
+            array(
                 'key' => 'field_faq_shortcode_info',
                 'label' => 'Shortcode à utiliser',
                 'name' => 'faq_shortcode_info',
@@ -171,7 +178,7 @@ function faq_acf_local_fields() {
         ),
         'menu_order' => 0,
         'position' => 'normal',
-        'style' => 'default',
+        'style' => 'seamless',
         'label_placement' => 'top',
         'instruction_placement' => 'label',
         'hide_on_screen' => '',
